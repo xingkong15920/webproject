8a6e68d0360be9ba80b7800cbb042e6066e662ef
longlei6666 <894640485@qq.com>
2019-10-07 16:50:27 +0800

    10.7更新安卓会员、旧版公共会员

---
+++ Projectpage/Vip-public/common/config.js
@@ -0,0 +1,344 @@
+//prd  线上		cs 测试   渠通宇104
+var Edition = 'zyh';
+var server;
+if(Edition == 'zyh1'){
+	server  = 'http://114.118.17.103:5002/'
+}else{
+	server  = 'http://api.51shanhe.com/m-crm/'
+}
+/* 
+接口前缀解释
+get 获取
+update 更新
+insert  插入
+del  删除
+*/
+var CmsConfig = {
+
+    ServiceUrl: {
+         //ApiRootUrl: 'http://10.31.141.200:8080/RTAEDS/',
+        //ApiRootUrl:'http://192.168.1.116:8080/IkkyuChegjtrack1/'
+        ApiRootUrl:server
+        // imgUrl:imgAdd
+    },
+    addressUrl: {
+        Login: {
+            // 获取验证码
+            createCode: "login/createCode",
+            // 登录
+            login: "login/loginJG",
+            //代理商登录
+            AgloginInfo:"login/AgloginInfo",
+            //退出
+            exitLogin:"login/exitLogin",
+            //自动登录
+            Automaticlist:"login/Automaticlist"
+        },
+        //机构后台接口
+        Mechanism: {
+            //---系统设置
+            //基本设置
+            //获取
+            getInstitutionRegisterInfo:"Institution/getInstitutionRegisterInfo",
+            //更新
+            updateInstitutionRegisterInfo:"Institution/updateInstitutionRegisterInfo",
+            //邮件设置
+            getInstitutionMailboxSetting:"InstitutionMailboxSetting/getInstitutionMailboxSetting",
+            updateInstitutionMailboxSetting:"InstitutionMailboxSetting/updateInstitutionMailboxSetting",
+            insertInstitutionMailboxSetting:"InstitutionMailboxSetting/insertInstitutionMailboxSetting",
+            send:"mail/send",
+            //短信设置
+            getInstitutionSMSSetting:"InstitutionSMSSetting/getInstitutionSMSSetting",
+            updateSMSSetting:"InstitutionSMSSetting/updateSMSSetting",
+            insertSMSS:"InstitutionSMSSetting/insertSMSS",
+            //验证短信
+            Phone:"Verify/Phone",
+            //修改密码
+            updatePassword:"login/updatePassword",
+            //管理员管理列表
+            getInstitutionStaff:"InstitutionStaff/getInstitutionStaff",
+            //添加管理员
+            insertInstitutionStaff:"InstitutionStaff/insertInstitutionStaff",
+            insterRiskSetUp:"InstitutionRiskSetUp/insterRiskSetUp",
+            updateRiskSetUp:"InstitutionRiskSetUp/updateRiskSetUp",
+            //修改管理员密码
+            updateInstitutionStaff:"InstitutionStaff/updateInstitutionStaff",
+            //删除管理员
+            
+            //--商户管理
+            //商户列表
+            getMerchantInfoList:"MerchantInfo/getMerchantInfoList",
+            //状态的更改三合一接口
+            updateMerchantStatus:"MerchantInfo/updateMerchantStatus",
+            // 操作接口：
+            //登录（登录标识）
+            //支付通道设置
+             //获取商户通道
+            getMerchantTD:"MerchantInfo/getMerchantTD",
+            //获取机构通道
+            getShopOfficial:"MerchantInfo/getOfficial",
+            //更新通道信息
+            updateMpType:"MerchantInfo/updateMpType",
+            //子账户列表
+            getMerchantBA:"MerchantInfo/getMerchantBA",
+            //操作
+            operationSub:"MerchantInfo/operationSub",
+            //添加子账户
+            insertInstitutionSub:"InstitutionSubaccount/insertInstitutionSub",
+            //风控设置
+            getMerRiskSetUp:"MerchantInfo/getMerRiskSetUp",
+            updateMerRiskSetUp:"MerchantInfo/updateMerRiskSetUp",
+            //费率设置
+            getMerchantRate:"MerchantInfo/getMerchantRate",
+            //费率修改
+            updateMerchantRate:"MerchantInfo/updateMerchantRate",
+            //密码设置
+            //编辑商户信息
+            updateMerchantInfo:"MerchantInfo/updateMerchantInfo",
+            //商户秘钥
+            //编辑商户秘钥
+            updateMerchantKeys:"MerchantInfo/updateMerchantKeys",
+            //删除商户
+
+            //添加商户
+            insertMerchantRegisterInfo:"MerchantRegister/insertMerchantRegisterInfo",
+            
+            //风控设置
+            getRiskSetUp:"InstitutionRiskSetUp/getRiskSetUp",
+            //登录记录
+            getMerchantLoginLog:'MerchantLoginLog/getMerchantLoginLog',
+            //商户操作日志，列表
+            getMerchantOperationLog:"MerchantOperationLog/getMerchantOperationLog",
+
+            //--代理设置
+            getAgentRegisterlist:"AgentRegister/getAgentRegisterlist",
+            updateAgentRegister:"AgentRegister/updateAgentRegister",
+            /*代理列表
+            
+            操作接口：
+            登录(登录方法？)
+            费率查询与修改
+            密码修改接口
+            编辑接口
+            删除代理接口
+            
+            */
+            //获取费率
+            getAgentRate:"AgentRegister/getAgentRate",
+            //修改费率
+            updateAgentRate:"AgentRegister/updateAgentRate",
+            //添加代理接口
+            insertAgentRegister:"AgentRegister/insertAgentRegister",
+            //资金变动列表接口
+
+            //--全部订单
+            getTransactionReceiptList:"TransactionReceipt/getTransactionReceiptList",
+            //获取机构下所有通道
+            getPassageList:"InstitutionOfficial/getPassageList",
+            //全部订单查询
+            //列表操作接口：查看、同步
+            //成功订单、未支付订单、异常订单同上
+
+
+            //--通道管理
+            //风控实况列表接口
+            getSubaccountRiskManagementList:"subaccountRiskManagement/getSubaccountRiskManagementList",
+            //通道管理列表查询
+            //操作：通道状态的改变接口
+            //编辑通道风控
+            updateRisk:"InstitutionOfficial/updateRisk",
+            //子账号接口
+            //查询子商户
+            getInstitutionSub:"InstitutionSubaccount/getInstitutionSub",
+            //添加子账户
+           insertInstitutionSub:"InstitutionSubaccount/insertInstitutionSub",
+           //模糊查询
+           getMerchantNumberlist:"InstitutionSubaccount/getMerchantNumberlist",
+           //编辑子账号
+           updateInstitutionSub:"InstitutionSubaccount/updateInstitutionSub",
+            //子账号风控编辑接口
+            updateInstitutionSubRisk:"InstitutionSubaccount/updateInstitutionSubRisk",
+
+            //获取子账户风控
+            getgetSubacSrk:"InstitutionSubaccount/getgetSubacSrk",
+            //产品接口
+            getRate:"InstitutionOfficial/getRate",
+            //修改产品
+            updateRate:"InstitutionOfficial/updateRate",
+            //获取通道详细信息(wechat,alipay)
+            getGFOfficial:"InstitutionOfficial/getGFOfficial",
+            //编辑接口(wechat,alipay)
+            updateGFInstitutionOfficial:"InstitutionOfficial/updateGFInstitutionOfficial",
+            //查询第三方通道
+            getThirdpartyOfficial:"InstitutionOfficial/getThirdpartyOfficial",
+            //编辑第三方通道
+            updateThirdpartyOfficial:"InstitutionOfficial/updateThirdpartyOfficial",
+            //删除接口
+            updateType:"InstitutionOfficial/updateType",
+
+            
+            //通道配置接口
+            //通道列表
+            getOfficial:"InstitutionOfficial/getOfficial",
+            //添加官方通道(微信)
+            insertWXOfficial:"/InstitutionOfficial/insertWXOfficial",
+            //查询官方通道(微信)
+            getWXOfficial:"InstitutionOfficial/getWXOfficial",
+            //更新支付宝
+            updateWeChat:"InstitutionOfficial/updateWeChat",
+            //添加官方通道(支付宝)
+            insertZFBOfficial:"InstitutionOfficial/insertZFBOfficial",
+            //查询官方通道(支付宝)
+            getZFBOfficial:"InstitutionOfficial/getZFBOfficial",
+            //更新支付宝通道
+            updateAlipay:"InstitutionOfficial/updateAlipay",
+            //添加第三方通道
+            insertThirdpartyTD:"InstitutionOfficial/insertThirdpartyTD",
+            //--广告管理
+            insertInstitutionAdvertisement:"InstitutionAdvertisement/insertInstitutionAdvertisement",
+             //广告列表"
+            getInstitutionAdvertisementList:"InstitutionAdvertisement/getInstitutionAdvertisementList",
+            //广告列表操作：
+            //状态的开/关
+            updateInstitutionAdvertisementState:"InstitutionAdvertisement/updateInstitutionAdvertisementState",
+            //编辑接口
+            updateInstitutionAdvertisement:"InstitutionAdvertisement/updateInstitutionAdvertisement",
+            //删除接口
+            delInstitutionAdvertisementState:"InstitutionAdvertisement/delInstitutionAdvertisementState",
+            
+
+
+            //代理商结算审核
+            getException:"Withdrawal/getException",
+            //查看代理商结算账单
+            GetSettlement:"Withdrawal/GetSettlement",
+            //更新代理结算
+            updateExType:"Withdrawal/updateExType",
+            //--财务分析
+            //查询当日统计
+            //商户订单金额TOP10列表
+            //商户订单笔数TOP10列表
+            //商户订单成功率TOP10列表
+            //查询30天交易订单概况折线图
+            //查询时间间隔展现折线图
+            //平台报表列表
+            //列表操作：冲正接口
+            //接口交易统计
+            //查询支付通道接口
+            //根据父级查询子级支付统计
+
+        },
+        //代理商后台
+        Agent: {
+            //订单管理
+            //查询订单
+            selectOrderList:"agOrderManngementController/selectOrderList",
+            //商户
+        	//查询商户信息
+            getMerchantlist:"AgMerchant/getMerchantlist",
+            //查询店铺
+            getShoplist:"AgEquipment/getShoplist",
+            //查询云喇叭和码牌
+            getEquipmentlist:"AgEquipment/getEquipmentlist",
+
+            
+
+
+            //销售
+            //查询销售列表
+            getSaleInfo:"saleManagement/getSaleInfo",
+            //添加销售
+            saleInsert:"saleManagement/insert",
+            //销售编辑
+            updateSale:"saleManagement/updateSale",
+
+            //代理
+            insertAgent:"AgManagement/insertAgent",
+            //查询列表
+            getAgent:"/AgManagement/getAgent",
+            //编辑代理
+            modifyAgent:"AgManagement/modifyAgent",
+
+            //提现
+            //查询提现账户
+            getBackCard:"Withdrawal/getBackCard",
+            //添加提现账户
+            insertBankCard:"Withdrawal/insertBankCard",
+            //查询提现记录
+            getAgentWithdrawCash:"Withdrawal/getAgentWithdrawCash",
+            //添加提现记录
+            insertAgentWithdrawCash:"Withdrawal/insertAgentWithdrawCash",
+            //查询代理商余额
+            getAgentBalance:"Withdrawal/getAgentBalance",
+
+
+            //码牌
+            //查询码牌列表
+            getCode:"AgEquipment/getCode",
+            //生成二维码
+            GenerateCode:"AgEquipment/GenerateCode",
+        },
+        //商户后台
+        Merchant: {
+            //交易数据
+            getTransactionList: "transaction/getTransactionList",
+            //交易数据--门店列表
+            getShopNames: "transaction/getShopNames",
+            //门店管理
+            getShops: "store/getShops",
+            //门店管理--添加门店
+            addShop: "store/addShop",
+            //门店管理--修改店铺
+            modifyShop: "store/modifyShop",
+            //店员管理
+            getClerks: "clerk/getClerks",
+            //店员管理--添加店员
+            addClerk: "clerk/addClerk",
+            //店员管理--修改店员
+            modifyClerk: "clerk/modifyClerk",
+            //店员管理--设备管理
+            getEquipments:"equipment/getEquipments",
+            //广告管理
+            getadvertlist:"advert/getadvertlist",
+            //广告管理--添加
+            addAdvert:"advert/addAdvert",
+            //广告管理--修改
+            modifyAdvert:"advert/modifyAdvert",
+            //广告管理--删除
+            deleteAdvert:"advert/deleteAdvert",
+            //广告管理--状态
+            modifyAdvertState:"advert/modifyAdvertState",
+            //账单管理--门店账单
+            getShopBill: "bill/getShopBill",
+            //账单管理--商户账单
+            getMerchantBill: "bill/getMerchantBill",
+            //数据统计
+            getStatistics:"statistics/getStatistics",
+        },
+        //会员
+        Vip: {
+        	//验证码
+        	login:"login/login",
+        	//会员详情
+        	getMember:"member/getMember",
+        	//查询充值记录
+        	selectRechargeRecord:"member/selectRechargeRecord",
+        	//查询消费记录
+        	selectConsumptionRecord:"member/selectConsumptionRecord",
+          	//获取openID
+          	getOpenId:"wechat/getOpenId",
+          	//绑定openID
+          	bindMemberOpenId:"wechat/bindMemberOpenId",
+          	//查询优惠券列表
+          	getCoupon:"member/getCoupon",
+        },
+        //公共
+        Public: {
+          getAddress:"login/getAddress",
+
+        },
+        
+       
+    }
+
+};
---
+++ Projectpage/Vip-public/js/pullToRefresh.js
@@ -0,0 +1,332 @@
+/**
+ * 为指定的容器添加滚动条,支持下拉刷新与上拉加载功能
+ * @param container 需要滚动的容器,要求设置css: position!=static,height=
+ * @param option 配置项,详见下方defaultOption说明
+ * @return 返回对象用于操控此区域,当前暴露了iscroll的refresh函数,当你在插件之外向滚动区域增加/删除内容后应该主动调用一次
+ * @description
+ *
+ * 2017-03-29
+ * 1）支持上拉加载
+ * 2017-03-30
+ * 1）改为jquery静态函数插件
+ * 2）支持关闭下拉刷新或上拉加载
+ */
+$.installPullToRefresh =
+function (container, option) {
+    // 触摸容器的手指集合
+    var touchFingers = {};
+    // 集合大小
+    var fingerCount = 0;
+
+    // 触摸事件的唯一ID
+    var touchEventID = 0;
+    // 刷新事件的唯一ID
+    var refreshEventID = 0;
+    // 当前的加载事件
+    var loadEvent = null;
+
+    // 当前图标位置
+    var curY = -55;
+
+    // 默认参数
+    var defaultOption = {
+        // 刷新相关
+        noRefresh: false, // 关闭下拉刷新特性
+        pauseBound: 40,  // 触发刷新的位置(也是图标loading暂停的位置)
+        lowerBound: 80, // 最大下拉到多少px
+        loadImg: "load.png", // loading图片
+        pullImg: "pull.png", // 下拉图片
+        onRefresh: function (refreshDone) { // 刷新数据回调
+            setTimeout(function() { // 默认不做任何事
+                refreshDone();
+            }, 0);
+        },
+
+        // 加载相关
+        noLoad: false, // 关闭上拉加载特性
+        bottomHeight: 1, // 距离滚动条底部多少px发起刷新
+        onLoad: function (loadDone) {
+            setTimeout(function() {
+                loadDone();
+            }, 0);
+        },
+    };
+    var finalOption = $.extend(true, defaultOption, option);
+
+    // 创建iscroll5滚动区域
+    var iscroll = new IScroll(container, {
+        bounce: false,
+    });
+
+    // XXX: touch event will lose after target is deleted, so check after user's DOM operation
+    function handleRemovedTarget() {
+        for (var identifier in touchFingers) {
+            var finger = touchFingers[identifier];
+            if (!$.contains($(container).get(0), finger.target)) {
+                // touch target has been removed, so clear it
+                delete touchFingers[identifier];
+                --fingerCount;
+            }
+        }
+    }
+
+    // 关闭上拉加载特性
+    if (!finalOption.noLoad) {
+        // 监听滚动结束事件,用于上拉加载
+        iscroll.on('scrollEnd', function () {
+            // 有滚动条的情况下,才允许上拉加载
+            if (iscroll.maxScrollY < 0) { // maxScrollY<0表明出现了滚动条
+                var bottomDistance = (iscroll.maxScrollY - iscroll.y) * -1;
+                // 距离底部足够近，触发加载
+                if (bottomDistance <= finalOption.bottomHeight) {
+                    // 当前没有刷新和加载事件正在执行
+                    if (!loadEvent && !refreshEventID) {
+                        loadEvent = {}; // 生成新的加载事件
+                        finalOption.onLoad(function (error, msg) {
+                            loadEvent = null; // 清理当前的加载事件
+                            handleRemovedTarget();
+                            // 延迟重绘滚动条
+                            setTimeout(function () {
+                                iscroll.refresh();
+                            }, 0);
+                        });
+                    }
+                }
+            }
+        });
+    }
+
+    // 关闭下拉刷新特性
+    if (!finalOption.noRefresh) {
+        // 紧邻滚动区域,容纳刷新图标
+        var pullContainer = $('<div class="pullContainer"></div>');
+        // 设置下拉条宽度与容器宽度一致
+        pullContainer.css("width", $(container).css("width"));
+        // 创建小图标
+        var pullToRefresh = $('<div class="pullToRefresh"><img src="' + finalOption.pullImg + '"></div>');
+        // 保留小图标的快捷方式
+        var pullImg = pullToRefresh.find("img");
+        // 小图标加入到容器
+        pullContainer.append(pullToRefresh);
+        // 小图标容器添加到滚动区域之前
+        $(container).before(pullContainer);
+        // 预加载loadImg
+        $('<img src="' + finalOption.loadImg + '">');
+
+        // 设置transform的函数
+        function cssTransform(node, content) {
+            node.css({
+                '-webkit-transform' : content,
+                '-moz-transform'    : content,
+                '-ms-transform'     : content,
+                '-o-transform'      : content,
+                'transform'         : content,
+            });
+        }
+
+        // 调整小图标位置,角度,透明度
+        function goTowards(translateY, rotate, opcaticy) {
+            // 更新当前小图标的位置，获取css(transform)比较麻烦,所以每次变更时自己保存
+            curY = translateY;
+
+            // 旋转图标（根据抵达lowerBound的比例旋转,最大转1圈)
+            if (rotate === undefined) {
+                rotate = (curY / finalOption.lowerBound) * 360;
+            }
+            // 透明度根据抵达pauseBound的比例计算
+            if (opcaticy === undefined) {
+                opcaticy = (curY / finalOption.pauseBound) * 1;
+                if (opcaticy > 1) {
+                    opcaticy = 1;
+                }
+            }
+            // 改变位置和旋转角度
+            cssTransform(pullToRefresh, "translateY(" + translateY + "px) translateZ(0)" + "rotateZ(" + rotate + "deg)");
+            // 改变透明度
+            pullToRefresh.css("opacity", opcaticy);
+        }
+
+        // 开启回弹动画
+        function tryStartBackTranTop() {
+            // 启动回弹动画
+            pullToRefresh.addClass("backTranTop");
+            // 判断是否触发刷新
+            if (curY >= finalOption.pauseBound) {
+                goTowards(finalOption.pauseBound);
+                // 回弹动画结束发起刷新
+                pullToRefresh.on('transitionend webkitTransitionEnd oTransitionEnd', function (event) {
+                    // 由于transitionend会对每个属性回调一次,所以只处理其中一个
+                    if (event.originalEvent.propertyName == "transform") {
+                        // 暂停动画
+                        pullToRefresh.removeClass("backTranTop");
+                        pullToRefresh.unbind();
+                        // 透明度重置为1
+                        goTowards(finalOption.pauseBound, undefined, 1);
+                        // 切换图片为loading图
+                        pullImg.attr("src", finalOption.loadImg);
+                        // 因为anamition会覆盖transform的原因,使用top临时定位元素
+                        pullToRefresh.addClass("loadingAnimation");
+                        pullToRefresh.css("top", finalOption.pauseBound + "px");
+                        // 回调刷新数据,等待用户回调
+                        finalOption.onRefresh(function (error, msg) {
+                            // 用户回调时DOM通常已经更新, 需要通知iscroll调整（官方建议延迟执行，涉及到浏览器重绘问题）
+                            setTimeout(function () {
+                                iscroll.refresh();
+                            }, 0);
+                            handleRemovedTarget();
+                            // 重置角度,切换为pull图
+                            goTowards(finalOption.pauseBound);
+                            // 取消animation,重置top
+                            pullToRefresh.removeClass("loadingAnimation");
+                            pullToRefresh.css("top", "");
+                            // 延迟过渡动画100毫秒,给浏览器重绘的机会
+                            setTimeout(function () {
+                                // 切换为pull图
+                                pullImg.attr("src", finalOption.pullImg);
+                                // 恢复动画
+                                pullToRefresh.addClass("backTranTop");
+                                // 刷新完成
+                                refreshEventID = 0;
+                                // 弹回顶部
+                                goTowards(-55);
+                                // 滚动条回顶部
+                                iscroll.scrollTo(0, 0, 0);
+                            }, 100);
+                        });
+                    }
+                });
+            } else {
+                goTowards(-55); // 弹回顶部
+                refreshEventID = 0; // 未达成刷新触发条件
+            }
+        }
+
+        // 更新最新的手指集合
+        // 浏览器对changedTouches的实现存在问题, 因此总是使用全量的touches进行比对
+        function compareTouchFingers(event) {
+            var identSet = {};
+
+            // 添加target内新出现的手指
+            for (var i = 0; i < event.originalEvent.targetTouches.length; ++i) {
+                var touch = event.originalEvent.targetTouches[i];
+                identSet[touch.identifier] = true;
+                if (touchFingers[touch.identifier] === undefined) {
+                    touchFingers[touch.identifier] = { clientY: touch.clientY, target: touch.target};
+                    ++fingerCount;
+                }
+            }
+            // 将target内消失的手指移除
+            for (var identifier in touchFingers) {
+                // 与本次touchevent属于同一个target,但是touchevent中已消失的手指,需要移除
+                if (identSet[identifier] === undefined && touchFingers[identifier].target === event.originalEvent.target) {
+                    delete(touchFingers[identifier]);
+                    --fingerCount;
+                }
+            }
+        }
+
+        // 统一处理
+        $(container).on("touchstart touchmove touchend touchcancel", function(event) {
+            var beforeFingerCount = fingerCount;
+            compareTouchFingers(event);
+
+            if (!beforeFingerCount && fingerCount) { // 开始触摸
+                ++touchEventID; // 新建触摸事件
+                if (!refreshEventID) {
+                    // 新建翻页事件
+                    refreshEventID = touchEventID;
+                    // 如果存在,则关闭回弹动画与相关监听
+                    pullToRefresh.removeClass("backTranTop");
+                    pullToRefresh.unbind();
+                    // 切换为pull图
+                    pullImg.attr("src", finalOption.pullImg);
+                }
+            } else if (beforeFingerCount && !fingerCount) { // 结束触摸
+                if (touchEventID != refreshEventID) { // 在前一个刷新未完成前进行了触摸,将被忽略
+                    return;
+                }
+                // 解锁iscroll向上拉动
+                iscroll.unlockScrollUp();
+
+                // 尝试启动回弹动画
+                tryStartBackTranTop();
+            } else if (beforeFingerCount) { // 正在触摸
+                // 计算每个变化的手指, 取变化最大的delta
+                var maxDelta = 0;
+                for (var i = 0; i < event.originalEvent.changedTouches.length; ++i) {
+                    var fingerTouch = event.originalEvent.changedTouches[i];
+                    if (touchFingers[fingerTouch.identifier] !== undefined) {
+                        var delta = fingerTouch.clientY - touchFingers[fingerTouch.identifier].clientY;
+                        if (Math.abs(delta) > Math.abs(maxDelta)) {
+                            maxDelta = delta;
+                        }
+                        touchFingers[fingerTouch.identifier].clientY = fingerTouch.clientY;
+                    }
+                }
+                if (touchEventID != refreshEventID) {
+                    return;
+                }
+
+                // 滚动条必须到达顶部,才开始下拉刷新动画
+                if (iscroll.y != 0) {
+                    return;
+                }
+
+                // 图标的目标位置
+                var destY = curY + maxDelta;
+                // 向下不能拉出范围
+                if (destY > finalOption.lowerBound) {
+                    destY = finalOption.lowerBound;
+                }
+                // 向上不能拉出范围
+                if (destY <= -55) {
+                    destY = -55;
+                }
+                // 更新图标的位置
+                goTowards(destY);
+                // 一旦小图标进入视野, Y轴向上的滚动条将锁定
+                if (destY >= 0) {
+                    iscroll.lockScrollUp();
+                } else { // 一旦小图标离开视野, Y轴向上的滚动条释放锁定
+                    iscroll.unlockScrollUp();
+                }
+            }
+        });
+    }
+
+    // 初始化iscroll
+    setTimeout(function() {
+        iscroll.refresh();
+    }, 0);
+
+    // 返回操作此区域的工具对象
+    return {
+        // 用户如果在下拉刷新之外修改了滚动区域的内容，需要主动调用refresh
+        refresh: function() {
+            // 延迟以便配合浏览器重绘
+            setTimeout(function() {
+                iscroll.refresh();
+                handleRemovedTarget();
+            }, 0);
+        },
+        // 触发下拉刷新
+        triggerPull: function() {
+            // 正在刷新或者禁止刷新
+            if (refreshEventID || finalOption.noRefresh) {
+                return false;
+            }
+            // 滚动到顶部
+            iscroll.scrollTo(0, 0, 0);
+            // 暂停可能正在进行的最终阶段回弹动画
+            pullToRefresh.removeClass("backTranTop");
+            // 小图标移动到lowerbound位置
+            goTowards(finalOption.lowerBound);
+            // 创建新的刷新事件,占坑可以阻止在setTimeout之前的触摸引起刷新
+            refreshEventID = -1; // 负值可以忽略任何触摸事件,直到刷新完成
+            // 延迟到浏览器重绘
+            setTimeout(function() {
+                tryStartBackTranTop();
+            }, 100);
+        },
+    };
+};
